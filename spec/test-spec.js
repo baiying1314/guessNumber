3a254646af3a6c3ce1dfda05e3b26da13acec4d5
code <1165444511@qq.com>
2016-07-26 13:56:06 +0800

    CompareNumber class's third test and realization

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -1,7 +1,8 @@
+"use strict";
 const CompareNumber = require('../main/compareNumber');
 
 describe('guessNumber', ()=> {
-    const result = '1234'
+    const result = '1234';
     it(' class CompareNumber ---- 4A0B', ()=> {
         const input = '1234';
         const expectText = '4A0B';
@@ -16,6 +17,14 @@ describe('guessNumber', ()=> {
             const testNumber = new CompareNumber(input, result);
 
             expect(testNumber.compareNumber()).toEqual(expectText);
+    })
+
+    it('class Comparenumber ----xAxB',()=>{
+        const input = '1354';
+        const expectText = '2A1B';
+        const testNumber = new CompareNumber(input, result);
+
+        expect(testNumber.compareNumber()).toEqual(expectText);
     })
 })
 
